9859ca64836d256b41393b5d990ad5d684205c97
flyswatter <somniac@me.com>
2013-01-20 02:14:49 -0800

    Got it working.

--- index.js
+++ index.js
@@ -10,18 +10,21 @@ var user_input,
 	simulating=false,
 	tickCounter=0,
 	healthLossPerTic=0.1,
+	ticksToEat=210,
 	tickLength=15;
 
 $('button#simulate').click(function(ev){
 	ev.preventDefault()
-	console.log("Simulate clicked.")
+	//console.log("Simulate clicked.")
 	if(!simulating){
+		//console.log("Difficulty: "+$('#difficulty').val())
+		healthLossPerTic = parseFloat($('#difficulty').val())/1000
 		//alert("Beginning simulation!");
 		$('#simulate').html('End')
 		tickCounter=0
 		simulating=true
-		console.log(unescape($('#user_input').text()))
-		user_input = unescape($('#user_input').text())
+		console.log($('#user_input').val())
+		user_input = $('#user_input').val()
 		mainInterval = setInterval(function(){
 			tick()
 		},tickLength)
@@ -39,22 +42,17 @@ var philosopher = function(){
 		this.health=75.0,
 		this.enlightenment=100.0,
 		this.enlightenmentLossPerTic=0.001,
-		this.ticksToEat=3000,
 		this.eating=false,
 		this.timeSpentEating=0,
 		this.thinking=true,
 		this.ticksToThink=3000,
 		this.timeSpentThinking=0,
-		this.forksOnTable={
-			right:true,
-			left:true
-		}
 		this.forksInHand={
 			right:false,
 			left:false
 		}
 		this.behavior=function(philo){
-			console.log("Attmepting to eval")
+			//console.log("Attmepting to eval")
 			eval(user_input)
 		}
 		this.fate=function(){
@@ -64,13 +62,11 @@ var philosopher = function(){
 				this.eating = false;
 				this.forksInHand.left=false
 				this.forksInHand.right=false
-				this.forksOnTable.right=true
-				this.forksOnTable.left=true
 				philosophers[this.rightForkNum]="A Fork"
 				philosophers[this.leftForkNum]="A Fork"
 			}
 
-			if(this.eating) this.health += 100 / this.ticksToEat
+			if(this.eating) this.health += 100 / ticksToEat
 			if(this.thinking) this.enlightenment += 100/this.ticksToThink
 
 			if(!this.eating) this.health -= healthLossPerTic
@@ -78,49 +74,63 @@ var philosopher = function(){
 
 			if(this.eating){
 				this.eating = true
-				this.timeSpentEating = this.ticksToEat
+				this.timeSpentEating = ticksToEat
 			}
 			if(this.eating) this.timeSpentEating -= 1
 			if(this.timeSpentEating === 0) this.eating = false
-			if(!this.eating) this.health -= healthLossPerTic
+			//if(!this.eating) this.health -= healthLossPerTic
 			if(this.health<=0) {
-				alert(this.name+" died after "+tickCounter*tickLength/1000+" seconds! You lose!")
+				alert(this.name+" died after "+tickCounter*tickLength/1000+" weeks! You lose!")
 				clearInterval(mainInterval)
+				simulating=false
+				for(var l=0; l<philosophers.length;l++){
+					var phil=philosophers[l]
+					if(phil!==undefined&&phil.name!==undefined){
+						phil.health=75.0
+					}
+				}
 			}
 		}
 
 		//These would be the publicly disclosed 'api' functions:
 		this.pickUpFork=function(which){
+			//console.log(this.name+ " Trying to pick up a fork.")
 			if(which==="Left"){
-				if(philosophers[this.leftForkNum]==="A Fork"){
-					philosophers[this.leftForkNum]="Nothing."
+				//console.log("Wanting a left fork"+this.leftForkNum)
+				if(philosophers[this.leftForkNum]!==undefined){
+					console.log(this.name+" picking up a left fork, undefining "+this.leftForkNum)
+					philosophers[this.leftForkNum]=undefined
 					this.forksInHand.left = true
-					philosophers[this.leftForkNum-1].forksOnTable.left = true
-					console.log(this.name+" picked up "+which+" fork.")
-					if(this.forksInHand.right) this.isEating = true
+					//philosophers[this.leftForkNum-1].forksOnTable.right = false
+					//console.log(this.name+" picked up "+which+" fork.")
+					if(this.forksInHand.right) this.eating = true
 				}else{
 					return "Fork left of "+this.name+" is missing!"
 				}
 			}else{
-				if(philosophers[this.rightForkNum]==="A Fork"){
-					philosophers[this.rightForkNum]="Nothing."
+				if(philosophers[this.rightForkNum]!==undefined){
+
+					//console.log("picking up a right fork")
+					philosophers[this.rightForkNum]=undefined
 					this.forksInHand.right = true
-					console.log(this.name+" picked up "+which+" fork.")
-					philosophers[this.rightForkNum-1].forksOnTable.right = true
-					if(this.forksInHand.left) this.isEating = true
+					//console.log(this.name+" picking up a right fork, undefining "+this.rightForkNum)
+					//philosophers[this.rightForkNum-1].forksOnTable.left = true
+					if(this.forksInHand.left) this.eating = true
 				}else{
 					return "Fork right of "+this.name+" is missing!"
 				}
 			}
 		}
-		this.holdingFork=function(which){
+		this.isForkOnTable=function(which){
+			//console.log("Checking if holding "+which +" which is "+ this.forksOnTable.left+this.forksOnTable.right)
 			if(which==="Left"){
-				return this.forksOnTable.left
+				//console.log("Left activated"+(philosophers[this.leftForkNum]!==undefined))
+				return philosophers[this.leftForkNum]!==undefined
 			}else{
-				return this.forksOnTable.right
+				return philosophers[this.rightForkNum]!==undefined
 			}
 		}
-		this.isForkOnTable=function(which){
+		this.holdingFork=function(which){
 			if(which==="Left"){
 				return this.forksInHand.left
 			}else{
@@ -132,7 +142,9 @@ var philosopher = function(){
 		}
 		this.lifeRemaining=function(){
 			return this.health
-		}
+		},
+		this.leftForkNum=0
+		this.rightForkNum=0
 }
 
 var philosopherNames = [
@@ -140,23 +152,19 @@ var philosopherNames = [
 ]
 
 var philosophers = []
-for(var i = 0; i<philosopherNames.length; i++){
+for(var i = 0; i<10; i++){
 	var philosopherName = philosopherNames[i]
 	var newPhilosopher = new philosopher()
 	newPhilosopher.name=philosopherName
+
+	philoNum = i*2
+
 	newPhilosopher.num=philosophers.length
-	philoNum = philosopher.num
+	newPhilosopher.rightForkNum = philoNum+1
 	if(philoNum === 0){
-		newPhilosopher.leftForkNum = philosophers.length-1; 
-		newPhilosopher.rightForkNum = philoNum+1
-	} 
-	if(philoNum === philosophers.length-1){
+		newPhilosopher.leftForkNum = 9; 
+	}else{
 		newPhilosopher.leftForkNum = philoNum-1
-		newPhilosopher.rightForkNum = philosophers.length
-	}
-	if (philoNum > 0 && philoNum < philosophers.length-1){
-		newPhilosopher.leftForkNum = philoNum-1
-		newPhilosopher.rightForkNum = philoNum+1
 	}
 	philosophers.push(newPhilosopher)
 	philosophers.push("A Fork")
@@ -164,13 +172,14 @@ for(var i = 0; i<philosopherNames.length; i++){
 
 function tick(){
 	tickCounter+=1
-	philosophers.forEach(function(philosopher){
-		if(philosopher.name){
+	for(var i = 0; i < philosophers.length; i++){
+		var philosopher = philosophers[i]
+		if(philosopher!==undefined&&philosopher.name!==undefined){
 			philosopher.behavior(philosopher)
 			philosopher.fate()
 		}
-	})
-	console.log(philosophers)
+	}
+	//console.log(philosophers)
 	refreshView()
 }
 function refreshView(){
@@ -178,10 +187,10 @@ function refreshView(){
 		if(philosophers[i]==="A Fork"){
 			$('#fork'+i).show()
 		}
-		if(philosophers[i]==="Nothing."){
+		if(philosophers[i]===undefined){
 			$('#fork'+i).hide()
 		}
-		if(philosophers[i].name){
+		if(philosophers[i]!==undefined&&philosophers[i].name){
 			if(philosophers[i].forksInHand.left){
 				$('#'+philosophers[i].name+' .leftArm img').show()
 			}else{
